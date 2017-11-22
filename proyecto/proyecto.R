c6c5f9cd728a7fe1d244a17b135aa796383784d9
JuanPabloRosas <juanpablo.rosasbaldazo@gmail.com>
2017-11-22 13:25:37 -0600

    avance de codigo

--- proyecto/proyecto.R
+++ proyecto/proyecto.R
@@ -1,4 +1,5 @@
 library(parallel)
+library(ggplot2)
 ##------------------------------------   LEE Y EXTRA DATOS PDDL --------------------------##
 lee = function(path) {
   archivo <- data.frame()
@@ -14,7 +15,8 @@ lee = function(path) {
   close(con)
   return(data.frame(archivo))
 }
-datos <-((lee("/home/pabloide/Documentos/3 Semestre/R_paralelo/proyecto/datos_p3.txt")))
+
+datos <-((lee("/home/pabloide/Documentos/3 Semestre/R_paralelo/proyecto/instancias/instancias_pequeñas/problem1m-1t-2s-5a-2020800U-4.pddl.txt")))
 colnames(datos)<-c(1: length(datos))
 datos <-t(datos)
 
@@ -36,9 +38,9 @@ part_sub <- matrix(subs,ncol = 2, byrow = TRUE)
 ##------------------------------------   OBTIENE A QUE TEMA PERTENECE  --------------------##
 tema <- matrix()
 for(s in 1:as.numeric(datos[2])){ #Debe ser hasta la cantidad de temas
-  c <- length(unlist(as.list(strsplit(datos[40 + s], " "))))
+  c <- length(unlist(as.list(strsplit(datos[9 + as.numeric(datos[3]) + 1 + s], " "))))
   for(i in 1:c){
-    tema <- c(tema,c(unlist(as.list(strsplit(datos[40 + s], " ")))[i],s)) 
+    tema <- c(tema,c(unlist(as.list(strsplit(datos[9 + as.numeric(datos[3]) + 1 + s], " ")))[i],s)) 
   }
 }
 tema <- na.omit(tema)
@@ -47,9 +49,9 @@ part_tema <- matrix(tema,ncol = 2, byrow = TRUE )
 ##-----------------------------------  OBTIENE A QUE MATERIA PERTENECE  -------------------##
 mate <- matrix()
 for(s in 1:datos[1]){ #Debe ser hasta la cantidad de materias
-  c <- length(unlist(as.list(strsplit(datos[46 + s], " "))))
+  c <- length(unlist(as.list(strsplit(datos[9 + as.numeric(datos[3]) + as.numeric(datos[2]) + 1 + s], " "))))
   for(i in 1:c){
-    mate <- c(mate,c(unlist(as.list(strsplit(datos[46 + s], " ")))[i],s)) 
+    mate <- c(mate,c(unlist(as.list(strsplit(datos[9 + as.numeric(datos[3]) + as.numeric(datos[2]) + 1 + s], " ")))[i],s)) 
   }
 }
 mate <- na.omit(mate)
@@ -59,8 +61,8 @@ part_mat <- matrix(mate,ncol = 2, byrow = TRUE )
 requiere <- matrix(c(0,0),nrow=as.numeric(datos[4]),ncol = 2)
 if(as.numeric(datos[5]) != 0){
   for(s in 1:datos[4]){ 
-    for(l in 1:datos[4]){#Debe ser hasta la cantidad de act con requisitos
-      c <- unlist(as.list(strsplit(datos[49 + l], " ")))
+    for(l in 1:datos[5]){#Debe ser hasta la cantidad de act con requisitos
+      c <- unlist(as.list(strsplit(datos[9 + as.numeric(datos[3]) + as.numeric(datos[2]) + as.numeric(datos[1]) + 3 + l], " ")))
       requiere[as.integer(c[1]),]<-c(c[1],c[2])
     }
   }
@@ -72,101 +74,84 @@ actividades <- as.numeric(actividades)
 actividades <- matrix(actividades, nrow = as.numeric(datos[4]))
 colnames(actividades) <- c("valor","duracion","recurso","sub","requiere")
 
+
 ##------------------------------------  MONTECARLO  -------------------------------------##
 
-montecarlo1 <- function(i){
-  cantidad <- round(as.numeric(datos[4]) * .68)  #Cantidad de subtemas como minimo
-  muestra <- sample(c(1:as.numeric(datos[4])),cantidad) #Cantidad de Actividades
-  return(muestra)
-}
-
 tiempos <- function(i){
-  act <- actividades[muestra[i,],]
+  act <- actividades[unlist(muestra[i]),]
   return(colSums(act[,1:2])[2])
 }
 
-puntaje <- function(i){
-  act <- actividades[muestra[i,],]
-  cuantos <- length(muestra[i,])
-  puntos <- matrix(0,nrow = as.numeric(datos[3]))
-  for(j in 1:cuantos){ # Cantidad de subtemas
-    a<-act[j,]
-    puntos[a[4]] <- puntos[a[4]] + a[1]
-  }
-  return(puntos)
-}
-
-valida <- function(i){
-  for(c in 1:as.numeric(datos[3])){
-    if(puntaje[i,c] < 70){
-      return(0)
+montecarlo1 <- function(i){
+  seleccion <- data.frame() # Guarda las actividades seleccionadas
+  puntos <- matrix(0,nrow = as.numeric(datos[3])) #Inicializa matriz de puntos por sub
+  for(n in 1:as.numeric(datos[3])){
+    muestra <- matrix(actividades[actividades[,4] == n], ncol=5)
+    while(puntos[n] < 70){ # Restriccion mayor a 70 por subtema
+      i <- sample(length(muestra[,1]),1)
+      a<-muestra[i,]
+      muestra <- muestra[-i,]
+      puntos[a[4]] <- puntos[a[4]] + a[1]
+      cual <- which(apply(actividades, 1, function(actividades) all(actividades == a)))
+      seleccion <- rbind(seleccion, cual)
     }
   }
-  return(i)
+  return(seleccion)
 }
-
-promedio <- function(i){
-  return(sum(puntaje[i,])/ as.numeric(datos[3]))
-}
+    
 ##----------------------------------- EXPERIMENTACION  ----------------------------------##
-##////             PARALELO
-repeticiones <- 1000000
+
+d <- data.frame()
+mejores <- list()
 cluster <- makeCluster(detectCores() - 1)
-clusterExport(cluster, "datos")
-clusterExport(cluster, "actividades")
-clusterExport(cluster, "montecarlo1")
-muestra <- parSapply(cluster, 1:repeticiones, montecarlo1)
-muestra <- t(muestra)
-
-clusterExport(cluster, "tiempos")
-clusterExport(cluster, "muestra")
-clusterExport(cluster, "actividades")
-tiempo <- parSapply(cluster, 1:repeticiones, tiempos)
-
-clusterExport(cluster, "puntaje")
-puntaje <- parSapply(cluster, 1:repeticiones, puntaje)
-puntaje <- t(puntaje)
-
-png("/home/pabloide/Documentos/3 Semestre/R_paralelo/proyecto/puntaje_subtema.png")
-boxplot(puntaje, xlab="Subtemas", ylab="Puntaje")
-abline(h = 70, col="red")
-graphics.off()
-
-clusterExport(cluster, "valida")
-clusterExport(cluster, "puntaje")
-clusterExport(cluster, "datos")
-valida <- parSapply(cluster, 1:repeticiones, valida)
-
-
-clusterExport(cluster, "promedio")
-clusterExport(cluster, "puntaje")
-clusterExport(cluster, "datos")
-promedio <- parSapply(cluster, 1:repeticiones, promedio)
+tamaños <- data.frame()
+iteraciones <- 10
+for(h in 1:iteraciones){
+  print(h)
+  ##////             PARALELO
+  repeticiones <- 3628800 * .1 # Tardo 40 min para 10 iteraciones
+  clusterExport(cluster, "datos")
+  clusterExport(cluster, "actividades")
+  clusterExport(cluster, "montecarlo1")
+  muestra <- parSapply(cluster, 1:repeticiones, montecarlo1)
+  
+  clusterExport(cluster, "tiempos")
+  clusterExport(cluster, "muestra")
+  clusterExport(cluster, "actividades")
+  tiempo <- parSapply(cluster, 1:repeticiones, tiempos)
+    
+  #nombre <- paste0("/home/pabloide/Documentos/3 Semestre/R_paralelo/proyecto/soluciones/",h,"_sol.png")
+  #png(nombre)
+  #plot(promedios, tiempo, xlab = "Puntaje promedio", ylab="Tiempo", xlim = c(25,150), ylim = c(2300,3000), main = h)
+  #abline(v = 100, col="red")
+  #graphics.off()
+  
+  planes <- data.frame(table(unlist(muestra)))
+  mejor <- data.frame(table(unlist(muestra[which.min(tiempo)])))
+  #------------  CUANTAS ACTIVIDADES SE NECESITAN PARA SATISFACER ---------------------
+  tamaño <- length(mejor[,1])
+  tamaños <- rbind(tamaños,tamaño)
+  #------------------------------------------------------------------------------------
+  mejores <- c(mejores ,data.frame(mejor$Var1))
+  d <- rbind(d,planes)
+}
 stopCluster(cluster)
 
-png("/home/pabloide/Documentos/3 Semestre/R_paralelo/proyecto/soluciones.png")
-plot(promedio, tiempo, xlab = "Puntaje promedio", ylab="Tiempo", xlim = c(65,130))
-abline(v = 100, col="red")
+dat <- data.frame()
+for(i in 1:iteraciones){
+  dat <- rbind(dat,mejores[i])
+}
+
+capture.output(print(mejores), file = "/home/pabloide/Documentos/3 Semestre/R_paralelo/proyecto/mejores.csv")
+repite <-data.frame(table(dat))
+repite <- repite[order(repite$Freq, decreasing=T),]
+nombre <- paste0("/home/pabloide/Documentos/3 Semestre/R_paralelo/proyecto/mejores_act.png")
+png(nombre)
+ggplot(data=repite, aes(x=repite$dat, y=repite$Freq, group=1)) +  geom_line(color="red")+  geom_point()
 graphics.off()
 
-d <- valida[valida != 0]
-muestra <- muestra[d,]
-tiempo <- tiempo[d]
-if(is.integer(d)){
-  puntaje <- puntaje[d,]
-  
-  promedio <- data.frame()
-  for(p in 1:length(d)){
-    promedio <- rbind(promedio, sum(puntaje[p,])/ as.numeric(datos[3]))
-  }
-  colnames(promedio) <- "promedio"
-
-  png("/home/pabloide/Documentos/3 Semestre/R_paralelo/proyecto/factibles.png")
-  plot(promedio$promedio, tiempo, xlab = "Puntaje promedio", ylab="Tiempo", xlim = c(65,130))
-  abline(v = 100, col="red")
-  graphics.off()
-}
-##-------------------------------------------------------
-
-
-
+#________________________________________________________________________________________________________
+#files <- list.files(path="/home/pabloide/Documentos/3 Semestre/R_paralelo/proyecto/instancias/instancias_pequeñas", pattern=".txt", all.files=T, full.names=T)
+#for(file in files){
+#  print(file)
+#}
